c166df4ddb559541f62ba68d462ac84c789d309e
chenlong <1583479514@qq.com>
2020-07-30 18:08:04 +0800

    feat: update code

--- components/scratch/index.js
+++ components/scratch/index.js
@@ -2,4 +2,32 @@ import { createNamespace } from '../../src/utils'
 
 const [createComponent, bem] = createNamespace('scratch')
 
-export default createComponent({})
+export default createComponent({
+  props: {},
+
+  render () {
+    return (
+      <div
+        class={
+          bem('scratch-coupon')
+        }
+        id="scratch"
+        onTouchstart={this.startScratch}
+      >
+        <div
+          id="card"
+          ref="scratch"
+          class={
+            bem('scratch-card')
+          }
+        >
+          <img
+            vShow={false}
+            // src={}
+            alt=""
+          />
+        </div>
+      </div>
+    )
+  }
+})
